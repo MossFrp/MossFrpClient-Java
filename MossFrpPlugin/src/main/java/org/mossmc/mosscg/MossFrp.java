97ce39a5fd96769c7991f070e33661c008407622
MossCG <77914339+MossCG@users.noreply.github.com>
2021-10-30 21:39:14 +0800

    完工启动时语言设置，写了点注释
    RT

--- MossFrpPlugin/src/main/java/org/mossmc/mosscg/MossFrp.java
+++ MossFrpPlugin/src/main/java/org/mossmc/mosscg/MossFrp.java
@@ -1,4 +1,13 @@
 package org.mossmc.mosscg;
 
-public class MossFrp {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MossFrp extends JavaPlugin {
+    //主类
+    //插件版本先不急着写
+    //等我独立客户端版本搞完再开工
+    //只是新建文件夹表明我会写但是懒
+    @Override
+    public void onEnable() {
+    }
 }
--- MossFrpStandard/src/main/java/org/mossmc/mosscg/MossFrp.java
+++ MossFrpStandard/src/main/java/org/mossmc/mosscg/MossFrp.java
@@ -8,24 +8,42 @@ import java.util.Calendar;
 import java.util.Map;
 
 public class MossFrp {
+    //主类
+    //完成一堆文件检查与初始化
     public static void main(String[] args) {
         checkFile();
         loadConfig();
         loadLanguage();
-        sendInfo("检查完成！");
+        loadData();
+        sendInfo(getLanguage("Start_Welcome")+getData("version"));
+        sendInfo(getLanguage("Start_Copyright"));
     }
 
+    //yml数据全部转为Map存储在这一块
+    //方便调用所以写了get方法
     public static Map languageMap;
 
     public static Map configMap;
 
+    public static Map dataMap;
+
     public static String getLanguage(String languagePath) {
         return languageMap.get(languagePath).toString();
     }
+
     public static String getConfig(String configPath) {
         return configMap.get(configPath).toString();
     }
 
+    public static String getData(String dataPath) {
+        return dataMap.get(dataPath).toString();
+    }
+
+    //消息发送部分
+    //直接调用就可以发送消息
+    //别用System.out.printIn()了！！！
+    //包括后边try catch都是用这里的sendException
+    //不然log日志不记录
     public static void sendCommand(String command) {
         send(command,"command");
     }
@@ -44,17 +62,6 @@ public class MossFrp {
 
     public static void sendException(Exception exception) {
         exception(exception);
-    }
-
-    public static String getNowTime() {
-        Calendar rightNow = Calendar.getInstance();
-        int year = rightNow.get(Calendar.YEAR);
-        int month = rightNow.get(Calendar.MONTH)+1;
-        int day = rightNow.get(Calendar.DAY_OF_MONTH);
-        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
-        int minute = rightNow.get(Calendar.MINUTE);
-        int second = rightNow.get(Calendar.SECOND);
-        return year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second;
     }
 
     public static void send(String info,String type) {
@@ -78,27 +85,6 @@ public class MossFrp {
         logMessage(prefix+info);
     }
 
-    public static void logMessage(String message) {
-        fileInput(message);
-    }
-
-    public static FileWriter getWriter;
-
-    public static void loadWriter() throws IOException {
-        getWriter = new FileWriter("MossFrp/logs/latest.yml",true);
-    }
-    public static void fileInput(String info) {
-        try {
-            if (getWriter == null) {
-                loadWriter();
-            }
-            getWriter.write("["+getNowTime()+"] "+info+"\r\n");
-            getWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void exception(Exception e) {
         if (getConfig("exceptionMode").equals("light")) {
             send(e.getMessage(),"exception");
@@ -116,6 +102,45 @@ public class MossFrp {
         }
     }
 
+    //简单的获取时间的一个方法
+    //输出示例：2021-10-30-21-6-55
+    public static String getNowTime() {
+        Calendar rightNow = Calendar.getInstance();
+        int year = rightNow.get(Calendar.YEAR);
+        int month = rightNow.get(Calendar.MONTH)+1;
+        int day = rightNow.get(Calendar.DAY_OF_MONTH);
+        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        int minute = rightNow.get(Calendar.MINUTE);
+        int second = rightNow.get(Calendar.SECOND);
+        return year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second;
+    }
+
+    //log记录部分
+    //和send部分配套使用
+    public static FileWriter getWriter;
+
+    public static void loadWriter() throws IOException {
+        getWriter = new FileWriter("MossFrp/logs/latest.yml",true);
+    }
+
+    public static void logMessage(String message) {
+        fileInput(message);
+    }
+
+    public static void fileInput(String info) {
+        try {
+            if (getWriter == null) {
+                loadWriter();
+            }
+            getWriter.write("["+getNowTime()+"] "+info+"\r\n");
+            getWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //yml数据文件加载模块
+    //都是一个模子出来的，看的舒服
     public static void loadConfig() {
         Yaml yaml = new Yaml();
         FileInputStream input = null;
@@ -141,15 +166,34 @@ public class MossFrp {
         languageMap = yaml.loadAs(input, Map.class);
     }
 
+    public static void loadData() {
+        Yaml yaml = new Yaml();
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream("./MossFrp/languages/data.yml");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        dataMap = yaml.loadAs(input, Map.class);
+    }
+
+    //文件检查的方法
+    //启动的时候调用一遍
+    //用了一堆new不过问题不大
+    //有些地方没简化，还有优化空间
     public static void checkFile() {
+        //主文件夹检查
         File basicDir = new File("./MossFrp");
         if (!basicDir.exists()) {
             basicDir.mkdir();
         }
+        //log文件夹检查
         File logDir = new File("./MossFrp/logs");
         if (!logDir.exists()) {
             logDir.mkdir();
         }
+        //copy旧的log
         File latestLogFile = new File("./MossFrp/logs/latest.yml");
         File saveLogFile = new File("./MossFrp/logs/"+getNowTime()+".yml");
         if (latestLogFile.exists()) {
@@ -160,10 +204,7 @@ public class MossFrp {
                 e.printStackTrace();
             }
         }
-        File languageDir = new File("./MossFrp/languages");
-        if (!languageDir.exists()) {
-            languageDir.mkdir();
-        }
+        //config文件检查
         File configFile = new File("./MossFrp/config.yml");
         if (!configFile.exists()) {
             InputStream in = InputStream.class.getResourceAsStream("/config.yml");
@@ -175,12 +216,48 @@ public class MossFrp {
                 System.exit(1);
             }
         }
+        //语言文件夹检查
+        File languageDir = new File("./MossFrp/languages");
+        if (!languageDir.exists()) {
+            languageDir.mkdir();
+        }
+        //语言文件检查
         File languageFile = new File("./MossFrp/languages/zh_cn.yml");
         if (!languageFile.exists()) {
             InputStream in = InputStream.class.getResourceAsStream("/languages/zh_cn.yml");
             try {
                 assert in != null;
                 Files.copy(in, languageFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        languageFile = new File("./MossFrp/languages/en_us.yml");
+        if (!languageFile.exists()) {
+            InputStream in = InputStream.class.getResourceAsStream("/languages/en_us.yml");
+            try {
+                assert in != null;
+                Files.copy(in, languageFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //数据文件更新
+        File dataFile = new File("./MossFrp/languages/data.yml");
+        if (!dataFile.exists()) {
+            InputStream in = InputStream.class.getResourceAsStream("/data.yml");
+            try {
+                assert in != null;
+                Files.copy(in, dataFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            InputStream in = InputStream.class.getResourceAsStream("/data.yml");
+            try {
+                assert in != null;
+                Files.delete(dataFile.toPath());
+                Files.copy(in, dataFile.toPath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
