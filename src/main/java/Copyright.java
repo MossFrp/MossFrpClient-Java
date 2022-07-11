f6490415f9322860e556968729812bd2c02cdc30
commandf1 <76681129+commandf1@users.noreply.github.com>
2022-07-11 20:36:41 +0800

    final
    也许final会好点 我不知道 (

--- src/main/java/Copyright.java
+++ src/main/java/Copyright.java
@@ -1,3 +1,3 @@
 public class Copyright {
-    public static String Copyright = "本软件由MossFrp Team - B站@墨守MossCG版权所有";
+    public static final String Copyright = "本软件由MossFrp Team - B站@墨守MossCG版权所有";
 }
